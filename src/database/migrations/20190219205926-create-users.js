8edb6a595e6162e46e86a4f06eec70d841c14752
Luiz Guilherme Afonso de André <guilherme@MacBook-Air-de-Luiz.local>
2019-02-23 15:25:04 -0300

    Password criptography + user routes and controllers

--- src/database/migrations/20190219205926-create-users.js
+++ src/database/migrations/20190219205926-create-users.js
@@ -35,7 +35,7 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       },
-      updates_at: {
+      updated_at: {
         allowNull: false,
         type: Sequelize.DATE
       }
