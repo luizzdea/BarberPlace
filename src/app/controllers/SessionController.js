ff454dbcad51d49aeed391a2459464749e3b2a39
Luiz Guilherme Afonso de André <guilherme@MacBook-Air-de-Luiz.local>
2019-02-24 13:49:44 -0300

    Session and logout control auth middleware and flash messages

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -11,16 +11,25 @@ class SessionController {
     const user = await User.findOne({ where: { email } })
 
     if (!user) {
-      console.log('User not found')
+      req.flash('error', 'User not found')
       return res.redirect('/')
     }
 
     if (!(await user.checkPassword(password))) {
-      console.log('Wrong passwor')
+      req.flash('error', 'Wrong password')
       return res.redirect('/')
     }
 
+    req.session.user = user
+
     return res.redirect('/app/dashboard')
+  }
+
+  destroy (req, res) {
+    req.session.destroy(() => {
+      res.clearCookie('root')
+      return res.redirect('/')
+    })
   }
 }
 module.exports = new SessionController()
--- src/routes.js
+++ src/routes.js
@@ -6,12 +6,29 @@ const upload = require('multer')(multerConfig)
 
 const routes = express.Router()
 
-routes.get('/', SessionController.create)
+const authMiddleware = require('./app/middlewares/auth')
+const guestMiddleware = require('./app/middlewares/guest')
+
+routes.use((req, res, next) => {
+  res.locals.flashSuccess = req.flash('success')
+  res.locals.flashError = req.flash('error')
+
+  next()
+})
+
+routes.get('/', guestMiddleware, SessionController.create)
 routes.post('/signin', SessionController.store)
 
-routes.get('/signup', UserController.create)
+routes.get('/app/logout', SessionController.destroy)
+
+routes.get('/signup', guestMiddleware, UserController.create)
 routes.post('/signup', upload.single('avatar'), UserController.store)
 
-routes.get('/app/dashboard', (req, res) => res.render('dashboard'))
+routes.use('/app', authMiddleware)
+
+routes.get('/app/dashboard', (req, res) => {
+  console.log(req.session.user)
+  res.render('dashboard')
+})
 
 module.exports = routes
